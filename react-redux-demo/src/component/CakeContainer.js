64da182e608aaebac1180401d61d396890d2200a
Shiv Gaurav <shivgaurav27@gmail.com>
2020-12-10 10:55:19 +0530

    install dependencies

---
+++ react-redux-demo/src/component/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CakeContainer() {
+    return (
+        <div>
+            <h2>No. of cakes </h2>
+            <button>buy cakes </button>
+        </div>
+    )
+}
+
+export default CakeContainer
